f3cf252341b168f584949bac9eb3d077224ea6a3
Lovelynpearl <lovelynpearl@gmail.com>
2022-09-08 15:47:11 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - prints entry point
- * - Dora Korpar, 2015-10-19",
- * followed by a new line
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return: 1
  */
 int main(void)
 {
-     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-     return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
