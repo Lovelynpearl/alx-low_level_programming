89f7f190b5e2933f49c3f6888c1a250dc4accb99
Lovelynpearl <lovelynpearl@gmail.com>
2022-09-13 08:33:42 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <main.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(c[i]);
+	_putchar('\n');
+
+	return (0);
+}
