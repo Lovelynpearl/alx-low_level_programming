f832b790f60eebd36b038baeab19f22911367cac
Lovelynpearl <lovelynpearl@gmail.com>
2022-10-05 01:41:24 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * create_array - array for prints a string
  * @size: number elements array
@@ -11,6 +8,7 @@
 
 char *create_array(unsigned int size, char c)
 {
+
 char *buffer;
 unsigned int position;
 
